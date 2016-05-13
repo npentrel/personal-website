3427ffa24b751b3016259e828b298c871f94b4b2
npentrel <npentrel@gmail.com>
2016-05-13 23:52:27 +0100

    V1

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for, session, request, jsonify, render_template, send_from_directory
+
+app = Flask(__name__, static_url_path='', template_folder='static')
+
+@app.route('/')
+def index():
+    # return 'Hello World'
+    return redirect(url_for('static', filename='index.html'))
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # app.run(host='0.0.0.0')
