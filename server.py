3e7702e99ba2e37b65a30ed4681a237ce35f7686
npentrel <npentrel@gmail.com>
2017-01-21 20:13:53 -0500

    Obfuscating analytics link

--- server.py
+++ server.py
@@ -44,12 +44,12 @@ def link(source):
   return ('', 204)
 
 
-@app.route('/analytics')
+@app.route('/analyt1cs')
 def analytics():
   return render_table()
 
 
-@app.route('/analytics/<source>')
+@app.route('/analyt1cs/<source>')
 def filtered_analytics(source):
   return render_table(source)
 
