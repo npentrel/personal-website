f56d0fc3e5056a2723bc9d86b944c41faa5e461c
Naomi Pentrel <npentrel@gmail.com>
2018-02-11 20:54:19 +0000

    Add ssl

--- app/main.py
+++ app/main.py
@@ -84,4 +84,4 @@ def index():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, ssl_context=('cert.pem', 'key.pem'))
